2f657c6689eae07fa7bfb6b30ceef30fa441e732
Andreas Tampubolon <andreasjr61@gmail.com>
2019-09-26 20:20:44 +0700

    penambahan model product

--- Del-Kindle/src/main/java/com/medan/sitoluama/Del/Kindle/Model/Product.java
+++ Del-Kindle/src/main/java/com/medan/sitoluama/Del/Kindle/Model/Product.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import javax.persistence.*;
 
 @Entity
-@Table(name="Product")
+@Table(name="product")
 @EntityListeners(AuditingEntityListener.class)
 @JsonIgnoreProperties(value = {"createdAt", "updatedAt"}, allowGetters = true)
 public class Product {
@@ -16,14 +16,32 @@ public class Product {
     @Column(name ="id")
     private Long id;
 
-    @Column(name ="judul")
-    private String judul;
+    @Column(name ="title")
+    private String title;
 
-    @Column(name ="penerbit")
-    private String penerbit;
+    @Column(name ="description")
+    private String description;
 
-    @Column(name="harga")
-    private Integer harga;
+    @Column(name="picture_product")
+    private String picture_product;
+
+    @Column(name ="categories")
+    private String categories;
+
+    @Column(name ="publication_year")
+    private String publication_year;
+
+    @Column(name ="price")
+    private Integer price;
+
+    @Column(name ="author")
+    private String author;
+
+    @Column(name ="isbn")
+    private String isbn;
+
+    @Column(name ="publisher")
+    private String publisher;
 
 
     public Long getId() {
@@ -34,27 +52,75 @@ public class Product {
         this.id = id;
     }
 
-    public String getJudul() {
-        return judul;
+    public String getTitle() {
+        return title;
     }
 
-    public void setJudul(String judul) {
-        this.judul = judul;
+    public void setTitle(String title) {
+        this.title = title;
     }
 
-    public String getPenerbit() {
-        return penerbit;
+    public String getDescription() {
+        return description;
     }
 
-    public void setPenerbit(String penerbit) {
-        this.penerbit = penerbit;
+    public void setDescription(String description) {
+        this.description = description;
     }
 
-    public Integer getHarga() {
-        return harga;
+    public String getPicture_product() {
+        return picture_product;
     }
 
-    public void setHarga(Integer harga) {
-        this.harga = harga;
+    public void setPicture_product(String picture_product) {
+        this.picture_product = picture_product;
+    }
+
+    public String getCategories() {
+        return categories;
+    }
+
+    public void setCategories(String categories) {
+        this.categories = categories;
+    }
+
+    public String getPublication_year() {
+        return publication_year;
+    }
+
+    public void setPublication_year(String publication_year) {
+        this.publication_year = publication_year;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
     }
 }
--- Del-Kindle/src/main/java/com/medan/sitoluama/Del/Kindle/repository/ProductRepository.java
+++ Del-Kindle/src/main/java/com/medan/sitoluama/Del/Kindle/repository/ProductRepository.java
@@ -10,5 +10,5 @@ import java.util.List;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product,Long> {
-    List<Product> findById(long id);
+
 }
