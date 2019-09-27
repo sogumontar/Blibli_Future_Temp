0da6a9f16c5301e3c6295ac1ea63f0ddd145a942
Sogumontar Hendra Simangunsong <hendrasimz92@gmail.com>
2019-09-27 08:32:49 +0700

    detailProduct

--- FrontEnd/load.js
+++ FrontEnd/load.js
@@ -1,5 +1,18 @@
 function getAPI(){
 	let URL="https://swapi.co/api/starships"
+	let urlProduct="https://swapi.co/api/starships"
+	window.fetch(urlProduct).then((ress)=>{
+		return res.json()
+	}).then((ress)=>{
+		let d=ress.results
+		let s=d.map(i=>{
+			return{
+				manufacturer:i.manufacturer
+			}
+		})
+		let ht=gen(s)
+		window.divListss.innerHTML=ht
+	})
 
 	window.fetch(URL).then((res)=>{
 		return res.json()
@@ -12,11 +25,17 @@ function getAPI(){
 			}
 		})
 		let html=generateCastHtml(l)
+		let htmls=generateCastHtml(l)
 		window.divList.innerHTML=html
+		window.divLists.innerHTML=htmls
 	})
 }
 function generateCastHtml(list){
 	return list.map(i=>
 		`<li>${i.name}, ${i.mass}</li>`).join('')
+}
+function gen(list){
+	return list.map(i=>
+		`<li>${i.manufacturer}</li>`).join('')
 }
 getAPI()
