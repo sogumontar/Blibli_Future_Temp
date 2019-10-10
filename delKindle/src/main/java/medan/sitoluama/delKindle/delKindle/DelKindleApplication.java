e402c487f0262d9ffe9e38754480e4a3ac2ecf93
Andreas Tampubolon <andreasjr61@gmail.com>
2019-10-10 22:41:46 +0700

    first commit release/13-09-2019

---
+++ delKindle/src/main/java/medan/sitoluama/delKindle/delKindle/DelKindleApplication.java
@@ -0,0 +1,15 @@
+package medan.sitoluama.delKindle.delKindle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+public class DelKindleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DelKindleApplication.class, args);
+	}
+
+}
