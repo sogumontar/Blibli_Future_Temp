0991865f4f1eee33ec84a27b3e7b81e8c0d3017c
Andreas Tampubolon <andreasjr61@gmail.com>
2019-10-13 22:21:49 +0700

    add function login

--- delKindle/src/main/java/medan/sitoluama/delKindle/delKindle/DelKindleApplication.java
+++ delKindle/src/main/java/medan/sitoluama/delKindle/delKindle/DelKindleApplication.java
@@ -2,10 +2,8 @@ package medan.sitoluama.delKindle.delKindle;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing
 public class DelKindleApplication {
 
 	public static void main(String[] args) {
