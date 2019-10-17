1da2b2e7af93bd3e144c0ab49a5393361079d3a0
Sogumontar Hendra Simangunsong <hendrasimz92@gmail.com>
2019-10-17 20:24:32 +0700

    testLogin1

--- FrontEnd/js/script_register.js
+++ FrontEnd/js/script_register.js
@@ -1,28 +1,55 @@
-$(document).ready(function(){
+// $(document).ready(function(){
 
-  $("#register").click(function(){
-    assignUser();
-  });
+//   $("#register").click(function(){
+//     var URLS="http://localhost:9094/api/auth/signup"
 
-  function assignUser(){
-    var jsonVar = {
-        name: $("#name").val(),
-        email: $("#email").val(),
-        password: $("#password").val(),
-        username: $("#username").val()
-    };
-    $.ajax({
-              type:"POST",
-              url:"http://localhost:9094/api/auth/signup",
-              data: JSON.stringify(jsonVar),
-              contentType: "application/json",
-              success: function(data){
-                  console.log(data.message);
-              },
-              error: function(err) {
-                  $("#message").append(err.responseJSON.message);
-                  console.log(err.responseJSON.message);
-              }
-    });
-  }
-});
+//     asd();
+//   });
+
+//   function assignUser(){
+//     var jsonVar = {
+//         name: $("#name").val(),
+//         email: $("#email").val(),
+//         password: $("#password").val(),
+//         username: $("#username").val()
+//     };
+//     $.ajax({
+//               type:"POST",
+//               url:"http://localhost:9094/api/auth/signup",
+//               data: JSON.stringify(jsonVar),
+//               contentType: "application/json",
+//               success: function(data){
+//                   console.log(data.message);
+//               },
+//               error: function(err) {
+//                   $("#message").append(err.responseJSON.message);
+//                   console.log(err.responseJSON.message);
+//               }
+//     });
+//   }
+// });
+
+
+async function asd(){
+
+// The Endpoint URL
+let url = "http://localhost:9094/api/auth/signup"
+// Data to Post
+let data = {
+  name: 'foo',
+  email: 'bar@gmail.com', 
+  password: "asdf",
+  username: "yyuqwkd"
+  };
+// Request options
+await fetch(url, options)
+.then(function(response) {
+  // The Post Result
+})
+let options = {
+  method: 'POST',
+  body: JSON.stringify(data)
+  };
+ 
+
+}
