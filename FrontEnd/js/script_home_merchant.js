f7992d78ea99a4dbb1758c1779c42dbc3226b0b2
Sogumontar Hendra Simangunsong <hendrasimz92@gmail.com>
2019-11-02 11:57:03 +0700

    test

---
+++ FrontEnd/js/script_home_merchant.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+
+
+  assignDataProduct();
+
+  function assignDataProduct(){
+		$.ajax({
+          type:"GET",
+          contentType: "application/json",
+          url:"http://localhost:9094/product/",
+          success: function(data) {
+            var users = JSON.parse(JSON.stringify(data));
+            for (var i in users) {
+               $("#listBook").
+                append("<div class='col-lg-3 col-md-6 mb-4'>\
+                    <div class='card h-100'>\
+											<img class='card-img-top' src='http://placehold.it/500x325' alt=''>\
+											<div class='card-body'>\
+												<h4 class='card-title'><center><p>" + users[i].title+"</p></center></h4>\
+												<p class='card-text'>" + users[i].description+"</p>\
+											</div>\
+											<div class='card-footer'>\
+												<button class='btn btn-primary' id='but_beli'>Beli Sekarang</button>\
+											</div>\
+                    </div>\
+                  </div>");
+            }
+          },
+          error: function(data) {
+            console.log(data);
+            }
+        });
+	}
+});
---
+++ FrontEnd/js/script_login.js
@@ -0,0 +1,38 @@
+
+  function logs(){
+    var jsonVar = {
+      usernameOrEmail: document.getElementById("usernameOrEmail").value,
+      password: document.getElementById("password").value
+      
+    };
+    $.ajax({
+            type:"POST",
+            url:"http://localhost:9094/api/auth/signin",
+            data: JSON.stringify(jsonVar),
+            contentType: "application/json",
+            success: function(data){
+                alert("Berhasil Login");
+                console.log("andreas");
+                location.href = "index1.html";
+            },
+            error: function(err) {
+                alert("Username dan Password anda salah")
+                $("#message").append(err.responseJSON.message);
+                console.log(err.responseJSON.message);
+            }
+    });  
+  }
+  
+  function test(){
+    console.log("")
+  }
+  
+
+  // let response = await fetch('localhost:9094/api/auth/signin');
+  //
+  // if (response.ok) { // if HTTP-status is 200-299
+  //   // get the response body (the method explained below)
+  //   let json = await response.json();
+  // } else {
+  //   alert("HTTP-Error: " + response.status);
+  // }
