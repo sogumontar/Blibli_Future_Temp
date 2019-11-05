6e3c872cce5f0c1f41616f820d6a523d56a76143
Andreas Tampubolon <andreasjr61@gmail.com>
2019-11-05 11:23:41 +0700

    change catalog entry

---
+++ delKindle/src/main/java/com/example/template/controller/ProductController.java
@@ -0,0 +1,4 @@
+package com.example.template.controller;
+
+public class ProductController {
+}
---
+++ delKindle/src/main/java/com/example/template/repository/ProductRepository.java
@@ -0,0 +1,11 @@
+package medan.sitoluama.delKindle.delKindle.repository;
+
+
+import medan.sitoluama.delKindle.delKindle.Model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    Product findFirstById(Long id);
+}
