94dc1e32b7e2ec657ecfdf0af9bb9c83806b12cd
Andreas Tampubolon <andreasjr61@gmail.com>
2019-11-05 20:19:39 +0700

    add user interface

---
+++ FrontEnd/js/script_list_buku.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+
+  hideNavbar();
+
+  function hideNavbar(){
+		var prevScrollpos = window.pageYOffset;
+		window.onscroll = function() {
+		var currentScrollPos = window.pageYOffset;
+		  if (prevScrollpos > currentScrollPos) {
+		    document.getElementById("navbar").style.top = "0";
+		  } else {
+		    document.getElementById("navbar").style.top = "-50px";
+		  }
+		  prevScrollpos = currentScrollPos;
+		}
+	}
+}
