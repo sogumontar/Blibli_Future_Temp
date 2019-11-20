007266a2759d25006bedf589226d2c230465b213
Andreas Tampubolon <andreasjr61@gmail.com>
2019-11-20 07:41:53 +0700

    new front end

---
+++ FrontEnd/js/script_update_active.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+
+  var id = new URL(location.href).searchParams.get('id')
+
+  updateActive();
+  
+  function updateActive(){
+    $.ajax({
+          type:"POST",
+          headers: {
+              "Content-Type": "application/json",
+              // "Authorization": "Bearer "+c
+          },
+          url:"http://localhost:9080/user/updateStatusActive/"+id,
+          success: function(data) {
+            location.href="block_user.html";
+          },
+          error: function(data) {
+            console.log(data);
+            }
+        });
+  }
+});
