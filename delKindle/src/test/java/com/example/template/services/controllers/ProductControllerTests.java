c8a319bc030faa95be41ecee3efd111dc820d66d
unknown <hendrasimz92@gmail.com>
2019-12-09 13:41:45 +0700

    init branch

--- delKindle/src/test/java/com/example/template/services/controllers/ProductControllerTests.java
+++ delKindle/src/test/java/com/example/template/services/controllers/ProductControllerTests.java
@@ -18,27 +18,27 @@ import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
 
-//@RunWith(SpringRunner.class)
-//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ProductControllerTests {
-//
-//    @Value("${local.server.port}")
-//    private int port;
-//
-//    @MockBean
-//    private ProductService productService;
-//    private ObjectMapper mapper;
-//
-//    @Before
-//    public void setup() {
-//        RestAssured.port = port;
-//
-//        mapper = new ObjectMapper();
-//    }
-//    @After
-//    public void cleanup() {
-//        verifyNoMoreInteractions(productService);
-//    }
+
+    @Value("${local.server.port}")
+    private int port;
+
+    @MockBean
+    private ProductService productService;
+    private ObjectMapper mapper;
+
+    @Before
+    public void setup() {
+        RestAssured.port = port;
+
+        mapper = new ObjectMapper();
+    }
+    @After
+    public void cleanup() {
+        verifyNoMoreInteractions(productService);
+    }
 
 //    @Test
 //    public void testFindFirstById(){
